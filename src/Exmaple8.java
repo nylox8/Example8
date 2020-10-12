32b2aa5f4143a404c57d9e4a573cb8ceb8380a3b
nylox8 <codingontheeden@gmail.com>
2020-10-12 20:48:36 +0200

    Commit: Designed an algorithm that reads your age and then displays a message depending if the given age value is 18 or higher.

---
+++ src/Exmaple8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Example8 {
+    public static void main (String [] argv) {
+       int age;
+       System.out.println("Enter your age");
+
+       //Reading value
+        Scanner inputValue;
+        inputValue=new Scanner(System.in);
+        age = inputValue.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You have the legal age");
+        }
+    }
+}
